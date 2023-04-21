884a3f559a47fc9dd3b08b095a2736e7df9f7ab7
Bruno Seixas <102114046+iBrunos@users.noreply.github.com>
2023-04-21 00:09:46 -0300

    vc vai funcionar sim ou nao

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -1,10 +1,17 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDatabase = () => {
-    console.log("Wait connecting to the database")
+  console.log("Wait connecting to the database...");
 
-    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("MongoDb Atlas Connected"))
-    .catch((error) => console.log(error))
+  mongoose
+    .connect( "mongodb+srv://root:1234@cluster0.sbbxqd2.mongodb.net/?retryWrites=true&w=majority" ,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
+    .then(() => console.log("MongoDB Atlas Connected!"))
+    .catch((err) => console.log(`Error connecting to MongoDB Atlas: ${err}`));
 };
+
 export default connectDatabase;
